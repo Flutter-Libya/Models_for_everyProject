99d393e2c964504b123226526921061b3b491aa9
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2024-02-11 21:11:21 +0200

    Create Chat_Message_Model.dart

---
+++ Chat_Message_Model.dart
@@ -0,0 +1,12 @@
+class ChatMessage {
+  User sender;
+  String message;
+  DateTime timestamp;
+
+ 
+  ChatMessage({
+    required this.sender,
+    required this.message,
+    required this.timestamp,
+  });
+}
