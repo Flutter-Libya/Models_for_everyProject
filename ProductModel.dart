59387cccf065299321c5af706f90216b3f8c2e6c
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2024-02-11 20:25:18 +0200

    Create ProductModel.dart

---
+++ ProductModel.dart
@@ -0,0 +1,16 @@
+class Product {
+  String title;
+  String subtitle;
+  double price;
+  String distribution;
+  List<String> images;
+
+
+  Product({
+    required this.title,
+    required this.subtitle,
+    required this.price,
+    required this.distribution,
+    required this.images,
+  });
+}
